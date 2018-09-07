846554b4cf928f49cad9b15df9904676e046f18f
grt107 <31504327+grt107@users.noreply.github.com>
2018-09-07 16:02:23 +0200

    Add files via upload

--- grt-responsive-menu.js
+++ grt-responsive-menu.js
@@ -7,28 +7,40 @@
  * Released under the MIT license
 */
 
-// Prevent href clicks on dropdown category
-$('li.grt-dropdown > a').on('click', function(e){
-	e.preventDefault();
+// Create a function for mobile version
+(function( $ ){
+	$.fn.grtmobile = function () {
+		if ($(window).width() < 768) {
+			$('.grt-mobile-button').on('click', function(){
+				$(this).toggleClass("grt-mobile-button-open");
+				$("ul.grt-menu").toggleClass("open-grt-menu ");
+				$("html, body").toggleClass("body-overflow");
+			});
+			$('li.grt-dropdown').on('click', function(e){
+				$(this).toggleClass("active-dropdown");
+			});
+		}
+	}
+})( jQuery );
+
+// Initialize and check for mobile
+$.fn.grtmobile();
+
+// On resize window check for mobile
+$(window).resize(function(){
+	$.fn.grtmobile();
 });
 
-// Initialize stuff in mobile resolution
-if ($(window).width() < 768) {
-	$('.grt-mobile-button').on('click', function(){
-		$(this).toggleClass("grt-mobile-button-open");
-		$("ul.grt-menu").toggleClass("open-grt-menu ");
-		$("html, body").toggleClass("body-overflow");
-	});
-	$('li.grt-dropdown').on('click', function(e){
-		$(this).toggleClass("active-dropdown");
-	});
-}
-
-// Add shadow on scroll
+// Add shadow on scroll after 60px
 $(window).scroll(function(e){
    if ($(this).scrollTop() > 60){
        $('header').addClass('scrolled');
    } else {
        $('header').removeClass('scrolled');
    }
+});
+
+// Prevent a href clicks on dropdown category
+$('li.grt-dropdown > a').on('click', function(e){
+	e.preventDefault();
 });
