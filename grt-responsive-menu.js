36fb4268dfc7d6c481a5ff98e4af61c88ede272b
grt107 <31504327+grt107@users.noreply.github.com>
2018-09-07 14:30:59 +0200

    Add files via upload

--- grt-responsive-menu.js
+++ grt-responsive-menu.js
@@ -3,17 +3,25 @@
  * Version: 1.0
  * Author: GRT107
  *
- * Copyright (c) 2017 GRT107
+ * Copyright (c) 2018 GRT107
  * Released under the MIT license
 */
 
 
-// Initialize on click
-$('.grt-mobile-button').on('click', function(){
-    $(this).toggleClass("grt-mobile-button-open");
-    $("ul.grt-menu").toggleClass("open-grt-menu ");
-    $("body").toggleClass("body-overflow");
-});
+// Initialize stuff in mobile resolution
+if ($(window).width() < 768) {
+	$('.grt-mobile-button').on('click', function(){
+		$(this).toggleClass("grt-mobile-button-open");
+		$("ul.grt-menu").toggleClass("open-grt-menu ");
+		$("body").toggleClass("body-overflow");
+	});
+	$('li.grt-dropdown a').on('click', function(e){
+		e.preventDefault();
+	});
+	$('li.grt-dropdown').on('click', function(e){
+		$(this).children().next().toggleClass("active-dropdown");
+	});
+}
 
 // Add shadow on scroll
 $(window).scroll(function(e){
