662b90ed6ecbc195638e490f00d6f9ce11b0eb15
grt107 <31504327+grt107@users.noreply.github.com>
2020-04-29 14:41:39 +0200

    code optimized

--- grt-responsive-menu.js
+++ grt-responsive-menu.js
@@ -1,6 +1,6 @@
 /*!
  * GRT Responsive Menu - jQuery Plugin
- * Version: 1.0
+ * Version: 1.1
  * Author: GRT107
  *
  * Copyright (c) 2018 GRT107
@@ -10,33 +10,22 @@
 // Create a function for mobile version
 (function( $ ){
 	$.fn.grtmobile = function () {
-		if ($(window).width() < 768) {
-			$('.grt-mobile-button').on('click', function(){
-				$(this).toggleClass("grt-mobile-button-open");
-				$("ul.grt-menu").toggleClass("open-grt-menu ");
-				$("html, body").toggleClass("body-overflow");
-			});
-			$('li.grt-dropdown').on('click', function(e){
-				$(this).toggleClass("active-dropdown");
-			});
-		}
+		$('.grt-mobile-button').on('click', function(){
+			$(this).toggleClass("grt-mobile-button-open");
+			$("ul.grt-menu").toggleClass("open-grt-menu ");
+			$("html, body").toggleClass("body-overflow");
+		});
+		$('li.grt-dropdown').on('click', function(e){
+			$(this).toggleClass("active-dropdown");
+		});
 	}
 })( jQuery );
 
 // Initialize and check for mobile
 $.fn.grtmobile();
 
-// On resize window check for mobile
-var resizeTimer;
 
-$(window).on('resize', function(e) {
-  clearTimeout(resizeTimer);
-  resizeTimer = setTimeout(function() {
-     $.fn.grtmobile();
-  }, 250);
-})
-
-// Add shadow on scroll after 60px
+// Add fixed class on scroll after 60px
 $(window).scroll(function(e){
    if ($(this).scrollTop() > 60){
        $('header').addClass('scrolled');
@@ -45,7 +34,7 @@ $(window).scroll(function(e){
    }
 });
 
-// Prevent a href clicks on dropdown category
+// Prevent a href clicks on dropdown category item
 $('li.grt-dropdown > a').on('click', function(e){
 	e.preventDefault();
 });
